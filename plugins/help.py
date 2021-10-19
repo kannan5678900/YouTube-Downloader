b7051be751403c095df0891c8d0cb3ef7ba5a516
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-10-19 20:04:49 +0530

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -1,7 +1,7 @@
-from pyrogram import Client, Filters
+from pyrogram import Client, filters
 
 
-@Client.on_message(Filters.command(["help"]))
+@Client.on_message(filters.command(["help"]))
 async def start(client, message):
     helptxt = f"{message.from_user.first_name},ഒരു 𝗬𝗼𝘂𝗧𝘂𝗯𝗲 𝗨𝗿𝗹 ഇങ്ങോട്ട് തന്നെ😊..\n\n𝖢𝗎𝗋𝗋𝖾𝗇𝗍𝗅𝗒 𝖮𝗇𝗅𝗒 𝖲𝗎𝗉𝗉𝗈𝗋𝗍𝗌 𝖸𝗈𝗎𝖳𝗎𝖻𝖾 𝖲𝗂𝗇𝗀𝗅𝖾  (No playlist) 𝖩𝗎𝗌𝗍 𝖲𝖾𝗇𝗍 𝖸𝗈𝗎𝖳𝗎𝖻𝖾 𝖴𝗋𝗅"
     await message.reply_text(helptxt)
