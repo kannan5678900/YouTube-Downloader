4f21f9928e9ee4e6885a6310eed6bff6c243e742
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-10-19 20:41:11 +0530

    Update ytdlfunc.py

--- helper/ytdlfunc.py
+++ helper/ytdlfunc.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from pyrogram import Client, StopPropagation, filters 
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 import yt_dlp
+import youtube_dl
 from utils.util import humanbytes
 import asyncio
 
