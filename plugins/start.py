d0c8a4854f1c97aac29994dc65a1e8d89ddffe5b
kannan5678900 <85219809+kannan5678900@users.noreply.github.com>
2021-10-27 09:21:46 +0530

    Create start.py

--- plugins/start.py
+++ plugins/start.py
@@ -16,7 +16,7 @@ async def start(client, message):
         [InlineKeyboardButton("𝔻𝕖𝕧𝕖𝕝𝕠𝕡𝕖𝕣 📞", url="https://t.me/Peterparker6")],
         [InlineKeyboardButton("𝔸𝕕𝕕 𝕄𝕖 😊", url="https://t.me/ytdownloadv2bot?startgroup=true")]
     ])
-    welcomed = f"𝗛𝗲𝗹𝗹𝗼 {message.from_user.first_name}\n\n**I am the 𝐌𝐨𝐬𝐭 𝐀𝐝𝐯𝐚𝐧𝐜𝐞𝐝 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐁𝐨𝐭😂**[📥](https://telegra.ph/file/a6cd21396b2430384ecc9.png)\n\n/help 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 𝐈𝐧𝐟𝐨"
+    welcomed = f"𝗛𝗲𝗹𝗹𝗼 {message.from_user.first_name}\n\n**I am the 𝐌𝐨𝐬𝐭 𝐀𝐝𝐯𝐚𝐧𝐜𝐞𝐝 𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 𝐁𝐨𝐭😂**\n\n/help 𝐅𝐨𝐫 𝐌𝐨𝐫𝐞 𝐈𝐧𝐟𝐨"
     await client.send_photo(
         chat_id, 
         photo=f"{random.choice(PHOTO)}",
